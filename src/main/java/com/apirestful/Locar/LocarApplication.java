8a1eba273ac0f275ca2a9b668b6575121148e0de
Renaro <renaro_matos@hotmail.com>
2021-02-12 21:49:59 -0300

    Spring Initializer
    A partir daqui todos os commits devem ser feitos na develop

---
+++ src/main/java/com/apirestful/Locar/LocarApplication.java
@@ -0,0 +1,13 @@
+package com.apirestful.Locar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocarApplication.class, args);
+	}
+
+}
