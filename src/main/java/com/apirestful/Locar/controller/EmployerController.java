ae033c95905833b985a3dc90ba1ce2f4fa6e9bd7
Willian Giovanni <willianpires75@gmail.com>
2021-03-09 12:51:59 -0300

    feat teste

--- src/main/java/com/apirestful/Locar/controller/EmployerController.java
+++ src/main/java/com/apirestful/Locar/controller/EmployerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.apirestful.Locar.Services.EmployerService;
 import com.apirestful.Locar.model.Employer;
+import com.apirestful.Locar.repository.EmployerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +23,9 @@ public class EmployerController {
     @Autowired
     EmployerService employerService;
 
+    @Autowired
+    EmployerRepository employerRepository;
+
     @GetMapping("/employer")
     public List<Employer> listFuncionarios() {
         return employerService.findAll();
@@ -32,10 +36,16 @@ public class EmployerController {
         return employerService.findByCpf(cpf);
     }
 
+    // @PostMapping("/employer")
+    // public Employer saveFuncionario(@RequestBody Employer employer) {
+    //     employer.setAdmin(true);
+    //     return employerService.save(employer);
+    // }
+
     @PostMapping("/employer")
     public Employer saveFuncionario(@RequestBody Employer employer) {
         employer.setAdmin(true);
-        return employerService.save(employer);
+        return employerRepository.save(employer);
     }
 
     @DeleteMapping("/employer/{cpf}")
