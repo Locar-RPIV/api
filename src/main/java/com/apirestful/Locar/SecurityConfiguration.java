c5eff26b9de45a6e8eaeede2a2d198880f95e68a
Willian Giovanni <willianpires75@gmail.com>
2021-03-13 13:32:35 -0300

    feat cors

--- src/main/java/com/apirestful/Locar/SecurityConfiguration.java
+++ src/main/java/com/apirestful/Locar/SecurityConfiguration.java
@@ -1,27 +1,22 @@
 package com.apirestful.Locar;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 // @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private static final String[] AUTH_LIST = { "/api",
-                                                "/api/auth",
-                                                "/api/employer",
-                                                "/api/employer/{cpf}",
-                                                "/api/client",
-                                                "/api/client/{cpf}",
-                                                "/api/parther",
-                                                "/api/parther/{rg}",
-                                                "/api/automobile",
-                                                "/api/automobile/{id}",
-                                                "http://localhost:8080/swagger-ui.html.#/" };
+    private static final String[] AUTH_LIST = { "/api", "/api/auth", "/api/employer", "/api/employer/{cpf}",
+            "/api/client", "/api/client/{cpf}", "/api/parther", "/api/parther/{rg}", "/api/automobile",
+            "/api/automobile/{id}", "http://localhost:8080/swagger-ui.html.#/" };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,8 +24,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth ) throws Exception {
-        
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:8080/api/").allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
