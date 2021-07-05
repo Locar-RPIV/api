02ac474978608de073607434988c09ca647960f1
WillianGiovanni <willianpires75@gmail.com>
2021-07-05 16:17:48 -0300

    refactor: checking vehicle status before reservation

--- src/main/java/com/apirestful/Locar/controller/ReservationController.java
+++ src/main/java/com/apirestful/Locar/controller/ReservationController.java
@@ -9,8 +9,6 @@ import com.apirestful.Locar.model.Response;
 import com.apirestful.Locar.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,26 +34,27 @@ public class ReservationController {
 
     @GetMapping(value = "/reservation/{cpf}")
     public List<Reservation> cpfReserva(@PathVariable(value = "cpf") String cpf) {
-
         User user = reservaService.findByCpf(cpf);
-
         return reservaService.findByUser(user);
     }
 
     @PostMapping(value = "/reservation")
-    public Reservation saveReserva(@RequestBody Reservation reserva) {
+    public <Any> Any saveReserva(@RequestBody Reservation reserva) {
         String clientCpf = reserva.getUser().getCpf();
         User user = reservaService.findByCpf(clientCpf);
         reserva.setUser(user);
         int autoId = reserva.getVeiculo().getId();
         Automovel auto = reservaService.findByIdAutomovel(autoId);
+        if (auto.getStatus().toLowerCase().equals("indisponível")
+                || auto.getStatus().toLowerCase().equals("manutenção")) {
+            return (Any) new Response("Veiculo indisponível no momento.");
+        }
         reserva.setVeiculo(auto);
-        return reservaService.save(reserva);
+        return (Any) reservaService.save(reserva);
     }
 
     @PutMapping(value = "/reservation")
     public <Any> Any editReserva(@RequestBody Reservation reserva) {
-        Response response = new Response();
         try {
             Reservation updateReservation = reservaService.findById(reserva.getId());
             if (reserva.getUser() != null) {
@@ -73,23 +72,19 @@ public class ReservationController {
             if (reserva.getDataRetirada() != null)
                 updateReservation.setDataRetirada(reserva.getDataRetirada());
 
-            reservaService.save(updateReservation);
-            return (Any) new ResponseEntity<>(HttpStatus.OK);
+            return (Any) reservaService.save(updateReservation);
         } catch (Exception e) {
-            response.setMessage("Erro interno.");
-            return (Any) new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            return (Any) new Response("Erro ao alterar reserva.");
         }
     }
 
     @DeleteMapping(value = "/reservation/{id}")
     public <Any> Any deleteReserva(@PathVariable(value = "id") int id) {
-        Response response = new Response();
         try {
             reservaService.deleteById(id);
-            return (Any) new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return (Any) new Response("Reserva apagada.");
         } catch (Exception e) {
-            response.setMessage("Erro interno.");
-            return (Any) new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            return (Any) new Response("Erro ao apagar reserva.");
         }
     }
 
