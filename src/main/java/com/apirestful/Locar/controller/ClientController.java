3d8c383509bf52e55639fda947a6e97f31287280
WillianGiovanni <willianpires75@gmail.com>
2021-07-20 14:42:12 -0300

    fix heroku

--- src/main/java/com/apirestful/Locar/controller/ClientController.java
+++ src/main/java/com/apirestful/Locar/controller/ClientController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 @CrossOrigin(origins = "*")
 public class ClientController {
-
     @Autowired
     ClientService clientService;
 
